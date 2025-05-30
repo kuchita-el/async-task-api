45a7a587d23993d7a8f903af3f58525b3105232d
Kuchita El <el.kuchita@gmail.com>
2025-05-30 06:11:48 +0000

    NestJSにPrismaを統合

---
+++ webapi/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  //   async enableShutdownHooks(app: INestApplication) {
+  //     // $onの型が合わない場合はanyキャストで回避
+  //     (this as any).$on('beforeExit', async () => {
+  //       await app.close();
+  //     });
+  //   }
+}

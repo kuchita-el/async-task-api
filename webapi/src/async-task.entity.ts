cbcf093ef11885f97a168edd6125bc225f80c463
Kuchita El <el.kuchita@gmail.com>
2025-05-31 10:46:41 +0000

    状態遷移用の関数を作成

--- webapi/src/async-task.entity.ts
+++ webapi/src/async-task.entity.ts
@@ -36,3 +36,57 @@ export type AsyncTask =
   | RunningAsyncTask
   | CompletedAsyncTask
   | ErrorAsyncTask;
+
+// --- 状態遷移用純粋関数 ---
+
+/**
+ * 実行待ち→実行中への遷移
+ */
+export function startAsyncTask(
+  task: WaitingAsyncTask,
+  startedAt: Date,
+): RunningAsyncTask {
+  return {
+    id: task.id,
+    status: 'RUNNING',
+    createdAt: task.createdAt,
+    startedAt,
+    payload: task.payload,
+  };
+}
+
+/**
+ * 実行中→完了への遷移
+ */
+export function completeAsyncTask(
+  task: RunningAsyncTask,
+  completedAt: Date,
+): CompletedAsyncTask {
+  return {
+    id: task.id,
+    status: 'COMPLETED',
+    createdAt: task.createdAt,
+    startedAt: task.startedAt,
+    completedAt,
+    payload: task.payload,
+  };
+}
+
+/**
+ * 実行中→エラーへの遷移
+ */
+export function errorAsyncTask(
+  task: RunningAsyncTask,
+  errorMessage: string,
+  errorStack: string,
+): ErrorAsyncTask {
+  return {
+    id: task.id,
+    status: 'ERROR',
+    createdAt: task.createdAt,
+    startedAt: task.startedAt,
+    errorMessage,
+    errorStack,
+    payload: task.payload,
+  };
+}

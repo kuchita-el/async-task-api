500c7243abc7c72dab51008f6ffbade2c4d68f77
Kuchita El <el.kuchita@gmail.com>
2025-05-31 13:04:03 +0000

    不要なファイルを削除

--- webapi/src/app.module.ts
+++ webapi/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AsyncTaskModule } from './async-task/async-task.module';
-import { PrismaModule } from 'src/shared/prisma/prisma.module';
 
 @Module({
-  imports: [AsyncTaskModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AsyncTaskModule],
 })
 export class AppModule {}
